faac6e8d910873747e7e06bdb2db3776b0c8cced
Sai D <saidonepudi2004@gmail.com>
2025-04-06 11:22:47 -0500

    changed home page title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <div>
             <h1 className="mb-4 text-2xl font-extrabold text-gray-800 dark:text-white md:text-5xl lg:text-3xl"> 
                 <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-                    Welcome to Pet Health AI  
+                    Pet Health AI  
                 </span> - Your AI-Powered Pet Companion!
             </h1>
         </div>
